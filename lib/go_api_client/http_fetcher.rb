aa7cd043c4f5f1423046f49631246d2e7f33b05e
Bill DePhillips <bill.dephillips@gmail.com>
2014-07-27 20:06:27 -0700

    added ability to configure an external status reporter

--- lib/go_api_client/http_fetcher.rb
+++ lib/go_api_client/http_fetcher.rb
@@ -4,15 +4,33 @@ require 'net/https'
 module GoApiClient
   class HttpFetcher
 
+    attr_reader :status_reporter
+
+    class StatusReporter
+      def on_request_start(url, options)
+        @start_time = Time.now
+        puts "[GoApiClient] fetching #{url}"
+      end
+
+      def on_request_success(response, url, options)
+        seconds = Time.now - @start_time
+        puts "[GoApiClient] fetched #{url} in #{seconds}sec"
+      end
+    end
+
+    class QuietStatusReporter
+      def on_request_start(*args); end
+      def on_request_success(*args); end
+    end
+
     def initialize(options={})
       @username = options[:username]
       @password = options[:password]
+      @status_reporter = options[:status_reporter] || ENV['QUIET'] ? QuietStatusReporter.new : StatusReporter.new
     end
 
     def get(url, options={})
       uri = URI.parse(url)
-
-      puts "[DEBUG] fetching #{url}" unless ENV['QUIET']
 
       password = options[:password] || uri.password || @password
       username = options[:username] || uri.user     || @username
@@ -23,6 +41,8 @@ module GoApiClient
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = uri.scheme == 'https'
 
+      status_reporter.on_request_start(uri, options)
+
       res = http.start do |http|
         req = Net::HTTP::Get.new(uri.request_uri)
         req.basic_auth(username, password) if username || password
@@ -31,6 +51,7 @@ module GoApiClient
 
       case res
       when Net::HTTPSuccess
+        status_reporter.on_request_success(res, uri, options.merge(:url => uri))
         return res
       end
       res.error!
